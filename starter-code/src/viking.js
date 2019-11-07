2a4dfff6701b9ec9342ad85c168ba49e01b2bb6e
Cole Mooney <colemooney5@gmail.com>
2019-11-07 14:22:12 -0500

    JS Viking lab practice Viking and Soldier

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,74 +1,38 @@
 // Soldier
 class Soldier {
-    constructor(health,strength) {
-        this.health = health
-        this.strength = strength
+    constructor(healthArg, strengthArg){
+        this.health = healthArg
+        this.strength = strengthArg
     }
-    attack() {
+    attack(){
         return this.strength
     }
-
-    receiveDamage(damage) {
+    receiveDamage(damage){
         this.health -= damage
     }
-
 }
-
 
 // Viking
 class Viking extends Soldier {
-    constructor(name, health, strength){
-    super(health, strength);
-    this.name = name
+    constructor(nameArg, healthArg, strengthArg){
+        super(healthArg, strengthArg)
+        this.name = nameArg
     }
     receiveDamage(damage){
         this.health -= damage
         if (this.health <= 0){
            return `${this.name} has died in act of combat`
         } else {
-            return `${this.name} has received ${damage} points of damage`
+           return `${this.name} has received ${damage} points of damage`
         }
     }
     battleCry(){
-        return "Odin Owns You All!"
+        return 'Odin Owns You All!'
     }
 }
 
 // Saxon
-class Saxon extends Soldier{
-    constructor(health, strength){
-        super(health, strength)
-    }
-    receiveDamage(damage){
-        this.health -= damage
-        if (this.health <= 0){
-            return `A Saxon has died in combat`
-        } else {
-            return `A Saxon has received ${damage} points of damage`
-        }
-    }
-}
+class Saxon {}
 
 // War
-class War {
-    constructor(){
-        this.vikingArmy = [];
-        this.saxonArmy = [];
-    }
-    addViking(Viking){
-        this.vikingArmy.push(Viking)
-    }
-    addSaxon(Saxon){
-        this.saxonArmy.push(Saxon)
-    }
-    vikingAttack(){
-        this.viking = this.Viking
-    }
-    // saxonAttack(){
-    //     Viking.receiveDamage()
-    //     if (Viking.health <= 0){
-    //         Viking.pop
-    //     }
-    // }
-    // showStatus()
-}
+class War {}
